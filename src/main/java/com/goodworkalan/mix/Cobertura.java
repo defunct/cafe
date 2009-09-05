37cf1cc1bdc0dd6e34846baf9b766afcc577d33f
Alan Gutierrez <alan@blogometer.com>
2009-09-05 01:29:34 -0500

    Began to implement Cobertura tests.

--- src/main/java/com/goodworkalan/mix/Cobertura.java
+++ src/main/java/com/goodworkalan/mix/Cobertura.java
@@ -1,5 +1,15 @@
 package com.goodworkalan.mix;
 
-public class Cobertura {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.goodworkalan.go.go.Artifact;
+
+public class Cobertura {
+    public void execute() {
+        Artifact artifact = new Artifact("cobertura", "cobertura", "1.9rc1");
+        List<String> arguments = new ArrayList<String>();
+        arguments.add("java");
+        ProcessBuilder process = new ProcessBuilder();
+    }
 }
