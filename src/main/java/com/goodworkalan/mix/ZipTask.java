07d60078802cafb2dbe6455a9ef7c3960658c1db
Alan Gutierrez <alan@blogometer.com>
2009-09-18 06:33:21 -0500

    Updated ZipTask so it can be a base class.

--- src/main/java/com/goodworkalan/mix/ZipTask.java
+++ src/main/java/com/goodworkalan/mix/ZipTask.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -14,6 +16,13 @@ import com.goodworkalan.go.go.Task;
 
 @Command(parent = MixTask.class)
 public class ZipTask extends Task {
+    private final byte[] buffer = new byte[4098];
+
+    private ZipOutputStream out;
+    
+    /** Set of files added to the zip file. */
+    private final Set<String> seen = new HashSet<String>();
+
     /** The files to add to the zip file. */
     private final FindList findList = new FindList();
     
@@ -75,32 +84,53 @@ public class ZipTask extends Task {
         findList.addExclude(exclude);
     }
     
+    protected void addDirectory(String entryName) throws IOException {
+        if (!entryName.endsWith("/")) {
+            entryName = entryName + "/";
+        }
+        if (seen.contains(entryName)) {
+            throw new MixException(0);
+        }
+        ZipEntry zipEntry = new ZipEntry(entryName);
+        out.putNextEntry(zipEntry);
+        out.closeEntry();
+    }
+    
+    protected void addFile(File source, String entryName) throws IOException {
+        if (seen.contains(entryName)) {
+            throw new MixException(0);
+        }
+        byte[] buffer = this.buffer;
+        FileInputStream in = new FileInputStream(source);
+        ZipEntry zipEntry = new ZipEntry(entryName);
+        out.putNextEntry(zipEntry);
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+        out.closeEntry();
+        in.close();
+    }
+    
+    protected void addAdditionalEntries() throws IOException {
+    }
+
     @Override
     public void execute(Environment env) {
-        byte[] buffer = new byte[4098];
         try {
-            ZipOutputStream out = new ZipOutputStream(new FileOutputStream(outputFile));
+            out = new ZipOutputStream(new FileOutputStream(outputFile));
             out.setLevel(level);
             for (FindList.Entry entry : findList) {
                 for (String fileName : entry.getFileNames()) {
                     File source = new File(entry.getDirectory(), fileName);
                     if (source.isDirectory()) {
-                        ZipEntry zipEntry = new ZipEntry(fileName + "/");
-                        out.putNextEntry(zipEntry);
-                        out.closeEntry();
+                        addDirectory(fileName);
                     } else {
-                        FileInputStream in = new FileInputStream(source);
-                        ZipEntry zipEntry = new ZipEntry(fileName);
-                        out.putNextEntry(zipEntry);
-                        int read;
-                        while ((read = in.read(buffer)) != -1) {
-                            out.write(buffer, 0, read);
-                        }
-                        out.closeEntry();
-                        in.close();
+                        addFile(source, fileName);
                     }
                 }
             }
+            addAdditionalEntries();
             out.close();
         } catch (IOException e) {
             throw new MixException(0, e);
