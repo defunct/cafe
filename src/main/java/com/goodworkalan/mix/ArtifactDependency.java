54358405a386c334980cc91e94f5bf0e9f3d723f
Alan Gutierrez <alan@blogometer.com>
2009-10-10 00:52:31 -0500

    Javadoc for the Dependency objects.

--- src/main/java/com/goodworkalan/mix/ArtifactDependency.java
+++ src/main/java/com/goodworkalan/mix/ArtifactDependency.java
@@ -7,21 +7,52 @@ import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.go.go.PathPart;
 import com.goodworkalan.go.go.ResolutionPart;
 
+/**
+ * A single artifact dependency.
+ * 
+ * @author Alan Gutierrez
+ */
 public class ArtifactDependency implements Dependency {
+    /** The artifact. */
     private final Artifact artifact;
-    
+
+    /**
+     * Create a single artifact dependency.
+     * 
+     * @param artifact
+     *            The artifact.
+     */
     public ArtifactDependency(Artifact artifact) {
         this.artifact = artifact;
     }
-    
+
+    /**
+     * Get a collection that contains a single unresolved path part for the
+     * artifact.
+     * 
+     * @param project
+     *            The project.
+     */
     public Collection<PathPart> getPathParts(Project project) {
         return Collections.<PathPart>singletonList(new ResolutionPart(artifact));
     }
-    
+
+    /**
+     * Get a collection that contains the single artifact.
+     * 
+     * @param project
+     *            The project.
+     */
     public Collection<Artifact> getArtifacts(Project project) {
         return Collections.singleton(artifact);
     }
-    
+
+    /**
+     * A do nothing make task.
+     * 
+     * @param project
+     *            The project.
+     */
     public void make(Project project) {
     }
 }
--- src/main/java/com/goodworkalan/mix/RecipeDependency.java
+++ src/main/java/com/goodworkalan/mix/RecipeDependency.java
@@ -7,33 +7,67 @@ import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.go.go.PathPart;
 
 // FIXME OutputDepdenency and RecipeDependency
+/**
+ * A recipe dependency that includes the recipe output and also includes the
+ * dependencies of any recipes the given recipe depends upon.
+ * 
+ * @author Alan Gutierrez
+ */
 public class RecipeDependency implements Dependency {
-    private final String recipeName;
-    
-    public RecipeDependency(String recipeName) {
-        this.recipeName = recipeName;
+    /** The recipe name. */
+    private final String name;
+
+    /**
+     * Create a recipe dependency for the recipe with the given name.
+     * 
+     * @param name
+     *            The recipe name.
+     */
+    public RecipeDependency(String name) {
+        this.name = name;
     }
 
+    /**
+     * Create a collection of unexpanded path parts that includes the path parts
+     * of recipe productions and the path parts of all of the recipe
+     * dependencies.
+     * 
+     * @param project
+     *            The project.
+     */
     public Collection<PathPart> getPathParts(Project project) {
         Collection<PathPart> parts = new ArrayList<PathPart>();
-        Recipe recipe = project.getRecipe(recipeName);
+        Recipe recipe = project.getRecipe(name);
         parts.addAll(recipe.getProduce());
         for (Dependency dependency : recipe.getDependencies()) {
             parts.addAll(dependency.getPathParts(project));
         }
         return parts;
     }
-    
+
+    /**
+     * Create a collection of the unexpanded artifacts including all the
+     * artifact dependencies of any recipes this recipe depends upon.
+     * 
+     * @param project
+     *            The project.
+     */
     public Collection<Artifact> getArtifacts(Project project) {
         Collection<Artifact> artifacts = new ArrayList<Artifact>();
-        Recipe recipe = project.getRecipe(recipeName);
+        Recipe recipe = project.getRecipe(name);
         for (Dependency dependency : recipe.getDependencies()) {
             artifacts.addAll(dependency.getArtifacts(project));
         }
         return artifacts;
     }
-    
+
+    /**
+     * Make the recipe by calling the make method of the given project.
+     * 
+     * @param project
+     *            The project.
+     */
     public void make(Project project) {
-        project.make(recipeName);
+        project.make(name);
     }
 }
