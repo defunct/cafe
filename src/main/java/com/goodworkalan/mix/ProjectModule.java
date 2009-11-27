f642288f22dd18111efea718da74d885b5065119
Alan Gutierrez <alan@blogometer.com>
2009-11-26 21:45:30 -0600

    Removed dead comments.

--- src/main/java/com/goodworkalan/mix/ProjectModule.java
+++ src/main/java/com/goodworkalan/mix/ProjectModule.java
@@ -2,12 +2,7 @@ package com.goodworkalan.mix;
 
 import com.goodworkalan.mix.builder.Builder;
 
-// FIXME
 public class ProjectModule {
     public void build(Builder builder) {
-        // Put 80% default here.
-    }
-    
-    public void extend(Builder builder) {
     }
 }
