b8eb93d60191fa35d2b9caf46d1823c0d049ec69
Alan Gutierrez <alan@blogometer.com>
2010-05-31 08:31:11 -0500

    Version bump.

--- src/mix/java/com/goodworkalan/mix/mix/MixProject.java
+++ src/mix/java/com/goodworkalan/mix/mix/MixProject.java
@@ -8,7 +8,7 @@ public class MixProject implements ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces("com.github.bigeasy.mix/mix/0.1.3.10")
+                .produces("com.github.bigeasy.mix/mix/0.1.3.11")
                 .main()
                     .depends()
                         .include("com.github.bigeasy.spawn/spawn/0.+1.1")
