f11e701743de22fc2d1ab471a7c31d4f1aa5bded
Alan Gutierrez <alan@blogometer.com>
2010-06-01 21:33:19 -0500

    Properly matching files in purge.
     * Matching only files in the current project, was too lax in matching.

--- src/main/java/com/goodworkalan/mix/github/PurgeCommand.java
+++ src/main/java/com/goodworkalan/mix/github/PurgeCommand.java
@@ -4,6 +4,7 @@ import static com.goodworkalan.mix.Production.productionsByName;
 import static com.goodworkalan.mix.Production.productionsByQualifiedName;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.regex.Pattern;
 import com.goodworkalan.github4j.downloads.Download;
 import com.goodworkalan.github4j.downloads.GitHubDownloadException;
 import com.goodworkalan.github4j.downloads.GitHubDownloads;
+import com.goodworkalan.go.go.Argument;
 import com.goodworkalan.go.go.Command;
 import com.goodworkalan.go.go.Commandable;
 import com.goodworkalan.go.go.Environment;
@@ -25,6 +27,13 @@ import com.goodworkalan.mix.Project;
 @Command(parent = GitHubCommand.class)
 public class PurgeCommand implements Commandable {
     private final static Pattern GITHUB_GROUP = Pattern.compile("com\\.github\\.\\w[-\\w\\d]*\\.\\w[-\\w\\d]*");
+    
+    private final List<String> suffixes = new ArrayList<String>();
+    
+    @Argument
+    public void addSuffix(String suffix) {
+        suffixes.add(suffix);
+    }
 
     private boolean isGitHubProject(Artifact artifact) {
         return GITHUB_GROUP.matcher(artifact.getGroup()).matches();
@@ -34,11 +43,27 @@ public class PurgeCommand implements Commandable {
         String[] split = artifact.getGroup().split("\\.");
         return new String[] { split[2], split[3] };
     }
-    private final Pattern EXTRACT_VERSION = Pattern.compile("^(?:\\w[-_\\w\\d]*\\-)+((?:\\.?\\d+)+).*?$");
+    private final Pattern EXTRACT_VERSION = Pattern.compile("^(\\w[-_\\w\\d]*\\-)+((?:\\.?\\d+)+)(.*)$");
     
+    public boolean matches(String artifactName, String stem, String suffix) {
+        for (String match : suffixes) {
+            String[] split = match.split("/");
+            String testName = artifactName + "-";
+            String testSuffix = "." + split[0];
+            if (split.length == 2) {
+                testSuffix = "-" + split[0] + "." + split[1];
+            }
+            if (stem.equals(testName)) {
+                if (suffix.equals(testSuffix)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     private void purge(Environment env, Collection<Production> productions, String version) {
         GitHubConfig github = env.get(GitHubConfig.class, 1);
-//        GitHubDownloads downloads = new GitHubDownloads(github.login, github.token);
         for (Production production : productions) {
             Artifact artifact = production.getArtifact();
             env.debug("inspecting", artifact);
@@ -51,22 +76,19 @@ public class PurgeCommand implements Commandable {
                 env.debug("wrongLogin", artifact, repository[0]);
                 continue;
             }
-            String prefix = artifact.getName() + "-"; 
             Map<String, List<Download>> byVersion = new HashMap<String, List<Download>>();
             try {
                 for (Download download : GitHubDownloads.getDownloads(github.login, repository[1])) {
                     String fileName = download.getFileName();
-                    if (fileName.startsWith(prefix)) {
-                        Matcher matcher = EXTRACT_VERSION.matcher(fileName);
-                        if (matcher.matches()) {
-                            String number = matcher.group(1);
-                            List<Download> candidates = byVersion.get(number);
-                            if (candidates == null) {
-                                candidates = new ArrayList<Download>();
-                                byVersion.put(number, candidates);
-                            }
-                            candidates.add(download);
+                    Matcher matcher = EXTRACT_VERSION.matcher(fileName);
+                    if (matcher.matches() && matches(artifact.getName(), matcher.group(1), matcher.group(3))) {
+                        String number = matcher.group(2);
+                        List<Download> candidates = byVersion.get(number);
+                        if (candidates == null) {
+                            candidates = new ArrayList<Download>();
+                            byVersion.put(number, candidates);
                         }
+                        candidates.add(download);
                     }
                 }
             } catch (GitHubDownloadException e) {
@@ -90,6 +112,9 @@ public class PurgeCommand implements Commandable {
     }
 
     public void execute(Environment env) {
+        if (suffixes.isEmpty()) {
+            suffixes.addAll(Arrays.asList("jar", "sources/jar", "javadoc/jar", "dep"));
+        }
         env.debug("purge", env.remaining);
         Project project = env.get(Project.class, 0);
         List<Production> productions = project.getProductions();
